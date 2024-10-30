68ddac1f97f7ada2fc6e5b45386ce30975ae2817
Sebastian <andersen_sebastian@yahoo.dk>
2024-10-30 15:19:07 +0100

    Add docs for GraphComponents and ReadInput

--- Utils/ReadInput.py
+++ Utils/ReadInput.py
@@ -1,12 +1,30 @@
 from Utils.GraphComponents import Node, Edge
+
 class ReadInput: 
-    
+    """
+    Methods:
+        <b>read_input<b>: Reads the input file (is called in contructor)
+        <b>printNodes</b>: Print each node
+        <b>printEdges</b>: Print each edge
+
+
+    Variables
+        <b>nodes (list<Node>)</b>: List of nodes
+        <b>edges (list<Edge>)</b>: List of edges
+        <b>s</b>: Start node
+        <b>t</b>: End node
+        <b>r</b>: Cardinality of reds
+    """    
     def __init__(self) -> None:
         self.nodes = []
         self.edges = []
         self.read_input()
 
     def read_input(self): 
+        """
+        Reads input data for a graph file.
+        """
+
         n, m, self.r = map(int, input().split())
         self.s, self.t = input().split()
 
@@ -21,10 +39,16 @@ class ReadInput:
             self.edges.append(edge)
 
     def printNodes(self):
+        """
+        Prints each node
+        """
         for node in self.nodes:
             print(node)  
     
     def printEdges(self):
+        """
+        Prints each edge
+        """
         for edge in self.edges:
             print(edge)
     
