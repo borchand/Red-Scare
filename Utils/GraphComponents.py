ac899934fd4a604242d1f508c5029c12a9dc6568
Nicklas Koch Rasmussen <nicra@itu.dk>
2024-10-30 15:23:17 +0100

    Working on graph

--- Utils/GraphComponents.py
+++ Utils/GraphComponents.py
@@ -1,3 +1,17 @@
+import networkx as nx
+
+class Graph:
+
+    def __init__(self, nodes: list, edges: list, is_directed: bool) -> None:
+        
+        if (is_directed):
+            self.nxGraph = nx.from_edgelist(edges, create_using=nx.DiGraph)
+        else:
+            self.nxGraph = nx.from_edgelist(edges)
+        
+
+    
+
 class Node:
     def __init__(self, node: str) -> None:
         self.is_red = "*" in node
@@ -13,7 +27,10 @@ class Edge:
         self.u = edge_splitted[0]
         self.v = edge_splitted[1]
         self.is_directed = ">" in edge ## ->
-    
+
+    def toTuple(self) -> tuple:
+        return (self.u, self.v)
+
     def __str__(self) -> str:
         s = "->" if self.is_directed else "--"
         return f"Edge: {self.u} {s} {self.v}"
