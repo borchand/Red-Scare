a2e879f6d59fe107485116c6b17f27f75869775c
Nicklas Koch Rasmussen <nicra@itu.dk>
2024-10-30 15:30:25 +0100

    edge tuple list

--- Utils/ReadInput.py
+++ Utils/ReadInput.py
@@ -6,11 +6,13 @@ class ReadInput:
         <b>read_input<b>: Reads the input file (is called in contructor)
         <b>printNodes</b>: Print each node
         <b>printEdges</b>: Print each edge
+        <b>getEdgeList</b>: returns the edgeTupleList
 
 
     Variables
         <b>nodes (list<Node>)</b>: List of nodes
         <b>edges (list<Edge>)</b>: List of edges
+        <b>edgeTupleList (list<tuple(u, v)>)</b>: List of edges as tuple (used for networkx)
         <b>s</b>: Start node
         <b>t</b>: End node
         <b>r</b>: Cardinality of reds
@@ -18,6 +20,7 @@ class ReadInput:
     def __init__(self) -> None:
         self.nodes = []
         self.edges = []
+        self.edgeTupleList = []
         self.read_input()
 
     def read_input(self): 
@@ -37,6 +40,7 @@ class ReadInput:
         for _ in range(m):
             edge = Edge(input())
             self.edges.append(edge)
+            self.edgeTupleList.append(edge.toTuple())
 
     def printNodes(self):
         """
@@ -51,6 +55,9 @@ class ReadInput:
         """
         for edge in self.edges:
             print(edge)
+    
+    def getEdgeList(self) -> list :
+        return self.edgeTupleList
     
     def __str__(self) -> str:
         self.printNodes()
