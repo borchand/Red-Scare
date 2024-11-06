b63ec99167dfb51fc9fbde532e6b5aeba06ce7c5
Michal Rynowiecki <michal_rynowiecki@outlook.com>
2024-11-06 13:49:59 +0100

    Changed the flow function to handle all cases by using the many code for directed graphs without cycles

--- Some.py
+++ Some.py
@@ -2,6 +2,27 @@ from Utils.ReadInput import ReadInput, ReadFile, BaseRead
 from Utils.GraphComponents import Graph
 import Utils.data_files as files
 import networkx as nx
+from many import solve, bellman
+from pathlib import Path
+from interruptingcow import timeout
+
+'''
+Get input
+'''
+def readInputSome(path):
+  # Read input
+  readFile = ReadFile(path)
+  source = readFile.s
+  sink = readFile.t
+
+  # Nx graph
+  graph = readFile.toGraph()
+  gnx = graph.nxGraph
+
+  # List of red nodes
+  reds = [node.node for node in graph.Nodes if node.is_red]
+
+  return readFile, gnx, source, sink, reds
 
 '''
 For each of the red vertices, find the shortest path
@@ -13,9 +34,9 @@ def someShortestPathRed(graph, source, sink, reds):
     try:
       # try a path from source to a red node
 
-      print(nx.shortest_path(graph, source, redNode))
+      nx.shortest_path(graph, source, redNode)
       # try a path from red node to sink
-      print(nx.shortest_path(graph, redNode, sink))
+      nx.shortest_path(graph, redNode, sink)
       return True
 
     except nx.NetworkXNoPath:
@@ -35,43 +56,84 @@ In each iteration do the following:
 This becomes NP-hard for directed graphs, unless the first vertice is a red one,
 and there exists a path between it and the sink.
 '''
-def someFlowPathRed(ogGraph, source, sink, reds):
-  thisGraph = nx.DiGraph()
-  # Add flow capacity 1 to all edges
-  for u, v in ogGraph.edges():
-    thisGraph.add_edge(u, v, capacity=1)
-  
-  # Add a superSource and a superSink
-  thisGraph.add_node('superSource')
-  thisGraph.add_node('superSink')
-  
-  for redNode in reds:
-    # Check if the rednode is in the graph
-    if redNode in thisGraph.nodes: 
-    # Add new superSource with flow capacity edge val=2 connected to red node
-      thisGraph.add_edge('superSource', redNode, capacity=2)
 
-      # Add new superSink with flow capacity edges val=1 connected to the original
-      thisGraph.add_edge(source, 'superSink', capacity=1)
-      thisGraph.add_edge(sink, 'superSink', capacity=1)
+def someFlowPathRed(readFile, ogGraph, source, sink, reds):
+  try: nx.has_path(ogGraph, source, sink)
+  except nx.NodeNotFound:
+    return False
 
-      # Check if flow from superSource to superSink is equal to 2
-      try:
-        print(redNode + ': ' + str(nx.maximum_flow(thisGraph, 'superSource', 'superSink', capacity='capacity')))
-        if nx.maximum_flow(thisGraph, 'superSource', 'superSink')[0] == 2:
-          return True
-        else:
+  # If the graph is directed acyclic, use many
+  if nx.is_directed_acyclic_graph(ogGraph):
+    if many.solve(input) > 0:
+      return True
+    else:
+      return False
+
+  # If the graph is directed cyclic, it's NP-hard
+  elif nx.is_directed(ogGraph):
+    print("NP-hard")
+    return False
+  
+  # Otherwise, undirected
+  else:
+
+    thisGraph = nx.Graph()
+    # Add flow capacity 1 to all edges
+    for u, v in ogGraph.edges():
+      thisGraph.add_edge(u, v, capacity=1)
+
+    # Add a superSource and a superSink
+    thisGraph.add_node('superSource')
+    thisGraph.add_node('superSink')
+
+    for redNode in reds:
+      # Check if the rednode is in the graph
+      if redNode in thisGraph.nodes:
+      # Add new superSource with flow capacity edge val=2 connected to red node
+        thisGraph.add_edge('superSource', redNode, capacity=2)
+
+        # Add new superSink with flow capacity edges val=1 connected to the original
+        thisGraph.add_edge(source, 'superSink', capacity=1)
+        thisGraph.add_edge(sink, 'superSink', capacity=1)
+
+        # Check if flow from superSource to superSink is equal to 2
+        try:
+          #print(redNode + ': ' + str(nx.maximum_flow(thisGraph, 'superSource', 'superSink', capacity='capacity')))
+          if nx.maximum_flow(thisGraph, 'superSource', 'superSink')[0] == 2:
+            return True
+          else:
+            thisGraph.remove_edge('superSource', redNode)
+            thisGraph.remove_edge(source, 'superSink')
+            thisGraph.remove_edge(sink, 'superSink')
+
+        except nx.NetworkXUnfeasible:
+          # If the path wasn't there, remove the unneeded edges
           thisGraph.remove_edge('superSource', redNode)
           thisGraph.remove_edge(source, 'superSink')
           thisGraph.remove_edge(sink, 'superSink')
+          continue
+        # If a red node is not connected to anything, do nothing
 
-      except nx.NetworkXUnfeasible:
-        # If the path wasn't there, remove the unneeded edges
-        thisGraph.remove_edge('superSource', redNode)
-        thisGraph.remove_edge(source, 'superSink')
-        thisGraph.remove_edge(sink, 'superSink')
-        continue
-      # If a red node is not connected to anything, do nothing
+    else:
+      return False
 
-  else:
-    return False
+'''
+Run the flow function on every file in the data folder
+'''
+
+def main():
+  folder_path = Path('/content/data')
+
+  for file in folder_path.iterdir():
+    if file.is_file():
+      try:
+        with timeout(10, exception=RuntimeError):
+          input, gnx, source, sink, reds = readInputSome(file)
+          print(someFlowPathRed(input, gnx, source, sink, reds))
+          continue
+      except RuntimeError: 
+        print("didn't finish within 10 seconds")
+      print(False)
+
+if __name__ == "__main__":
+  main()
